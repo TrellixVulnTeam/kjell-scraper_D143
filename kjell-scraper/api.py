f19c22b79f4859c074648b72c40b0520a7063b35
coffedahl <coffedahl@hotmail.com>
2021-08-16 23:43:24 +0200

    Better Deployment

---
+++ kjell-scraper/api.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, request
+from kjell_api import getInfo
+
+main = Blueprint('main', __name__)
+
+@main.route('/api/get_products', methods=['GET','POST'])
+def get_products():
+    article_string = request.get_json('string')
+    json_list = []
+    product_list = getInfo(article_string['string'])
+    for x in product_list:
+        print(x.get_link())
+        json_list.append({'name':x.get_name(), 'article_number':x.get_article_number(), 'price':x.get_price(), 'link': x.get_link()})
+    return jsonify(json_list)
---
+++ kjell-scraper/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from kjell_api import getInfo
+
+app = Flask(__name__, static_folder="../kjell-frontend/build",
+              static_url_path='/')
+
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/api/get_products', methods=['GET', 'POST'])
+def get_products():
+    article_string = request.get_json('string')
+    json_list = []
+    product_list = getInfo(article_string['string'])
+    for x in product_list:
+        json_list.append({'name': x.get_name(), 'article_number': x.get_article_number(
+        ), 'price': x.get_price(), 'link': x.get_link()})
+    return jsonify(json_list)
