98e725ededdc5ab1055cc57e5e804e957f8d06a2
coffedahl <coffedahl@hotmail.com>
2022-02-03 00:52:20 +0100

    Styling

--- kjell-frontend/src/Statistics/components/FunFactCard.js
+++ kjell-frontend/src/Statistics/components/FunFactCard.js
@@ -4,7 +4,7 @@ const FunFactCard = (props) => {
     return (
         <div className="funfact-card">
             <h1>{props.title}</h1>
-            <h1>{props.value}</h1>
+            <h1 className="funfact-value">{props.value}</h1>
         </div>
     );
 }
